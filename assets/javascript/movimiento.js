7d29b74495ff1330f997f3ee554c93eeba269896
Loretta Agyemang <Loretta Agyemang>
2016-12-21 14:22:03 -0500

    started fleshing out js script

--- assets/javascript/movimiento.js
+++ assets/javascript/movimiento.js
@@ -1,1 +1,19 @@
 // Linked to HTML 
+$(document).ready(function() { 
+
+		// create variable to store user input in form 
+		var action = []; 
+
+	$("input[type='maker']").on("click", "button", function(){
+		var val = $("input[type='text']").val();
+		list.push(val);
+		localStorage.setItem()
+
+
+		return false; 
+	}
+
+
+
+
+}); 
