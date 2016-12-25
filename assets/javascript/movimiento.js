4fcafc5eb88db263cc846d4ed75e906dede90c84
Loretta Agyemang <Loretta Agyemang>
2016-12-25 18:35:51 -0500

    worked on jumbotron

--- assets/javascript/movimiento.js
+++ assets/javascript/movimiento.js
@@ -4,14 +4,13 @@ $(document).ready(function() {
 		// create variable to store user input in form 
 		var action = []; 
 
-	$("input[type='maker']").on("click", "button", function(){
-		var val = $("input[type='text']").val();
-		list.push(val);
-		localStorage.setItem()
+
+	$("#mbutton").on("click", "button", function(){
+		append("<button>"); 
 
 
 		return false; 
-	}
+	}); 
 
 
 
