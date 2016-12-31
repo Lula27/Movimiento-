65699d44a70253e5c7327a7f34f0b2e745264ce9
Loretta Agyemang <Loretta Agyemang>
2016-12-30 19:15:27 -0500

    trying to get generated buttons to be responsive

--- assets/javascript/movimiento2.js
+++ assets/javascript/movimiento2.js
@@ -1,16 +1,13 @@
 $(document).ready(function(giftastic) { 
 
-	// create variable to store user input in form 
-		var actions = ["dance", "sleep", "laugh"]; 
 
-
-	$("#mbutton").click(function(buttons) {
+	$('#mbutton').click(function(buttons) {
 
 	// Prevent page from refreshing!
 	buttons.preventDefault()
 		
-
-	var a = $(".form-control").val(); 
+	// recognizes value of text written in form 
+	var a = $('.form-control').val(); 
 
 	// See if text is grabbed 
 	// console.log(a); 
@@ -18,32 +15,36 @@ $(document).ready(function(giftastic) {
 	// Generate button function 
 	function renderButton() {
 
-	var b = $("<button>").text(a);
+	var b = $('<button>').text(a);
 
 	// Add attribute to jazz up botton 
-	b.addClass("btn btn-default");
+	b.addClass('btn btn-default');
+
+	// Add ID
+	$(b).attr('id', 'maker')
 
 	// Add button to btn-group div
-	$(".bcontainer").append(b);		
+	$('.bcontainer').append(b);		
 
 	}
 
-
 	renderButton(); 
 
+	}); 
 
 
-	// $(".bcontainer").append(b); 
-	// $.ajax({
+	// Generate Giffs after clicking on buttons -not working...why?!
+	$('#maker').click(function(makegiff) {
 
-			// url: "http://api.giphy.com/v1/gifs/search?q=" + $(".form-control").val() + "&api_key=dc6zaTOxFJmzC",  
-			// type: "GET",
-			// success: function(response) {
-			// 	console.log(response.data[0]);
-			// }
-	
+		makegiff.preventDefault()
+		alert("Hello"); 
 
-	})
+	});  
+
+
+
+
+
 
 });
      
